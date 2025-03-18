ecf05ec143507f2c22aba1ee40c747d202dfc0f6
Chetan-Katkar <chetankatkar0307@gmail.com>
2025-03-18 21:01:16 +0530

    responsiveness

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let username;
 username = window.prompt("Enter your name:");
 
 if(username){
-	document.getElementById("username").textContent = `Hello ${username}!`;
+	document.getElementById("username").textContent = `Hello , ${username}!`;
 }
